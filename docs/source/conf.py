a82772158d14627adfe004ca299eee3b33a7974a
Ilan Guenet <ilan.guenet@epita.fr>
2022-03-07 22:41:50 +0100

    up: source directory

---
+++ docs/source/conf.py
@@ -0,0 +1,103 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../my_module')) # FIXME: path to your module
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Project name' # FIXME
+copyright = '2022, Project name' # FIXME
+author = 'author name' # FIXME
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0' # FIXME
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.imgmath', 'numpydoc',
+              'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 'matplotlib.sphinxext.plot_directive']
+source_suffix = '.rst'
+master_doc = 'index'
+pygments_style = 'sphinx'
+
+# Latex as a svg
+imgmath_image_format='svg'
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'scipy'
+html_theme_path = ['_theme']
+#html_logo = '_static/scipyshiny_small.png'
+html_static_path = ['_static']
+html_theme_options = {
+    "edit_link": "true",
+    "sidebar": "right",
+    "scipy_org_logo": "true",
+    "rootlinks": [("http://scipy.org/", "Scipy.org"),
+                  ("http://docs.scipy.org/", "Docs")]
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+
+#------------------------------------------------------------------------------
+# Plot style
+#------------------------------------------------------------------------------
+
+plot_pre_code = """
+import numpy as np
+import scipy as sp
+np.random.seed(123)
+"""
+plot_include_source = True
+plot_formats = [('png', 96), 'pdf']
+plot_html_show_formats = False
+
+import math
+phi = (math.sqrt(5) + 1)/2
+
+font_size = 13*72/96.0  # 13 px
+
+plot_rcparams = {
+    'font.size': font_size,
+    'axes.titlesize': font_size,
+    'axes.labelsize': font_size,
+    'xtick.labelsize': font_size,
+    'ytick.labelsize': font_size,
+    'legend.fontsize': font_size,
+    'figure.figsize': (3*phi, 3),
+    'figure.subplot.bottom': 0.2,
+    'figure.subplot.left': 0.2,
+    'figure.subplot.right': 0.9,
+    'figure.subplot.top': 0.85,
+    'figure.subplot.wspace': 0.4,
+    'text.usetex': False,
+}
